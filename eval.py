f30c5f6d85b8ceda16aa669085da0a2931b6caf4
Rohith Reddy <rohithreddy024@gmail.com>
2018-12-15 15:09:29 +0530

    replaced mle based validation with rouge based validation

--- eval.py
+++ eval.py
@@ -35,50 +35,6 @@ class Evaluate(object):
         self.model.load_state_dict(checkpoint["model_dict"])
 
 
-    def valid_one_batch(self, batch):
-        enc_batch, enc_lens, enc_padding_mask, enc_batch_extend_vocab, extra_zeros, ct_e = get_enc_data(batch)
-        dec_batch, max_dec_len, dec_lens, target_batch = get_dec_data(batch)
-        enc_batch = self.model.embeds(enc_batch)
-        enc_out, enc_hidden = self.model.encoder(enc_batch, enc_lens)
-        prev_s = None
-        sum_temporal_srcs = None
-        # -------------------------------Summarization-----------------------
-        step_losses = []
-        s_t = (enc_hidden[0], enc_hidden[1])
-
-        for t in range(min(max_dec_len, config.max_dec_steps)):
-            x_t = dec_batch[:, t]
-            x_t = self.model.embeds(x_t)
-            final_dist, s_t, ct_e, sum_temporal_srcs, prev_s = self.model.decoder(x_t, s_t, enc_out, enc_padding_mask,
-                                                              ct_e, extra_zeros, enc_batch_extend_vocab, sum_temporal_srcs, prev_s)
-            target = target_batch[:, t]
-            log_probs = T.log(final_dist + config.eps)
-            step_loss = F.nll_loss(log_probs, target, reduction="none", ignore_index=self.vocab.word2id(data.PAD_TOKEN))
-            step_losses.append(step_loss)
-
-        losses = T.sum(T.stack(step_losses, 1), 1)
-        batch_avg_loss = losses / dec_lens
-        mle_loss = T.mean(batch_avg_loss)
-
-        return mle_loss.item()
-
-
-    def validIters(self):
-        self.setup_valid()
-        count = mle_total = 0
-        batch = self.batcher.next_batch()
-        while batch is not None:
-            with T.autograd.no_grad():
-                mle_loss = self.valid_one_batch(batch)
-            mle_total += mle_loss
-            count += 1
-            batch = self.batcher.next_batch()
-
-        mle_avg = mle_total/count
-
-        load_file = self.opt.load_model
-        print(load_file, "mle_loss:", "%.3f"%mle_avg)
-
     def print_original_predicted(self, decoded_sents, ref_sents, article_sents, loadfile):
         filename = "test_"+loadfile.split(".")[0]+".txt"
     
@@ -88,7 +44,7 @@ class Evaluate(object):
                 f.write("ref: " + ref_sents[i] + "\n")
                 f.write("dec: " + decoded_sents[i] + "\n\n")
 
-    def test_batch(self, print_sents = False):
+    def evaluate_batch(self, print_sents = False):
 
         self.setup_valid()
         batch = self.batcher.next_batch()
@@ -130,7 +86,7 @@ class Evaluate(object):
             self.print_original_predicted(decoded_sents, ref_sents, article_sents, load_file)
 
         scores = rouge.get_scores(decoded_sents, ref_sents, avg = True)
-        if self.opt.task == "get_full_scores":
+        if self.opt.task == "test":
             print(load_file, "scores:", scores)
         else:
             rouge_l = scores["rouge-l"]["f"]
@@ -140,26 +96,20 @@ class Evaluate(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--task", type=str, default="validate", choices=["validate","test","get_full_scores"])
+    parser.add_argument("--task", type=str, default="validate", choices=["validate","test"])
     parser.add_argument("--start_from", type=str, default="0020000.tar")
     parser.add_argument("--load_model", type=str, default=None)
     opt = parser.parse_args()
 
-    if opt.task == "validate" or opt.task == "test":
+    if opt.task == "validate":
         saved_models = os.listdir(config.save_model_path)
         saved_models.sort()
         file_idx = saved_models.index(opt.start_from)
         saved_models = saved_models[file_idx:]
-        if opt.task == "validate":
-            for f in saved_models:
-                opt.load_model = f
-                valid_processor = Evaluate(config.valid_data_path, opt)
-                valid_processor.validIters()
-        else:   #test
-            for f in saved_models:
-                opt.load_model = f
-                valid_processor = Evaluate(config.test_data_path, opt)
-                valid_processor.test_batch()
-    else:   #get_full_scores
+        for f in saved_models:
+            opt.load_model = f
+            eval_processor = Evaluate(config.valid_data_path, opt)
+            eval_processor.evaluate_batch()
+    else:   #test
         valid_processor = Evaluate(config.test_data_path, opt)
-        valid_processor.test_batch()
+        valid_processor.evaluate_batch()
